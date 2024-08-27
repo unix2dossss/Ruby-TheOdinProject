d96fd1d92ea10db403ee6ba4d8a4ca59fc930ba5
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-08-27 14:16:26 +1200

    Complete var Exercise

---
+++ name.rb
@@ -0,0 +1,13 @@
+puts "What is your name?"
+name = gets.chomp
+puts 'Hello ' + name
+
+10.times do
+  puts 'Hello ' + name
+end
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hello " + first_name + " " + last_name + "!"
